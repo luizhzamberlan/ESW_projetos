3637be1c39c5414bc81a04f0c6e1057cca08f03e
Luiz Henrique Zamberlan <45802276+LuiZamberlan@users.noreply.github.com>
2023-02-02 22:04:24 -0300

    Update conversor_binario.py

--- conversor_de_bases/conversor_binario.py
+++ conversor_de_bases/conversor_binario.py
@@ -6,7 +6,7 @@ indice = 0
 conversao_p_dec = 0
 
 # cálculo para conversão de base_binária
-for i in range(tamanho, -1, -1):
+for i in range(tamanho_da_lista, -1, -1):
     conversao_p_dec += int(numero_binario[indice]) * (2 ** i)
     indice += 1
 
